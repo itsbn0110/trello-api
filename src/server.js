18760c8598cb5ddb62b43cec9ba75a85a778ee17
itsbn0110 <itsbn610@gmail.com>
2024-10-29 21:30:11 +0700

    Init Server

--- src/server.js
+++ src/server.js
@@ -1,12 +1,32 @@
-const express = require("express");
+/**
+ * Updated by trungquandev.com's author on August 17 2023
+ * YouTube: https://youtube.com/@trungquandev
+ * "A bit of fragrance clings to the hand that gives flowers!"
+ */
 
-const app = express();
+import express from 'express'
+import { mapOrder } from '~/utils/sorts.js'
 
-const hostname = "localhost";
-const port = 8686;
+const app = express()
 
-app.get("/", (req, res) => res.send("<h1>Helllo Server from BaoNgo</h1>"));
+const hostname = 'localhost'
+const port = 8017
+
+app.get('/', (req, res) => {
+  // Test Absolute import mapOrder
+  console.log(mapOrder(
+    [ { id: 'id-1', name: 'One' },
+      { id: 'id-2', name: 'Two' },
+      { id: 'id-3', name: 'Three' },
+      { id: 'id-4', name: 'Four' },
+      { id: 'id-5', name: 'Five' } ],
+    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
+    'id'
+  ))
+  res.end('<h1>Hello World!</h1><hr>')
+})
 
 app.listen(port, hostname, () => {
-  console.log(`Trello App is running on http://${hostname}:${port}`);
-});
+  // eslint-disable-next-line no-console
+  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
+})
