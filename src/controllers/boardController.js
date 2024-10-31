1cd3bea93118099fb0f0a6ea91a643d6cc350214
itsbn0110 <itsbn610@gmail.com>
2024-11-01 00:08:36 +0700

    ControllerHandlingg/ErrorHandling

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,1 +1,15 @@
 // example controller
+import { StatusCodes } from 'http-status-codes';
+
+const createNew = async (req, res, next) => {
+  try {
+    console.log('req.body', req.body);
+    res.status(StatusCodes.CREATED).json({ message: 'Created New from Controller' });
+  } catch (e) {
+    next(e);
+  }
+};
+
+export const boardController = {
+  createNew
+};
