429589f11215c86b6b4197fc8699392072b41bea
itsbn0110 <itsbn610@gmail.com>
2024-11-07 12:30:07 +0700

    update number of characters

--- src/validations/cardValidation.js
+++ src/validations/cardValidation.js
@@ -8,7 +8,7 @@ const createNew = async (req, res, next) => {
   const correctCondition = Joi.object({
     boardId: Joi.string().required().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE),
     columnId: Joi.string().required().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE),
-    title: Joi.string().required().min(3).max(50).trim().strict()
+    title: Joi.string().required().min(3).max(256).strict()
   });
   try {
     // abortearly trả về tất cả lỗi validation
