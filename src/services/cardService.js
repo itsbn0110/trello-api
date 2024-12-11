8ec8588720aa8dc3af3124c4fc6a4a4b8e98d688
itsbn0110 <itsbn610@gmail.com>
2024-12-12 02:00:06 +0700

    Cardmembers-feature

--- src/services/cardService.js
+++ src/services/cardService.js
@@ -48,6 +48,9 @@ const update = async (cardId, reqBody, cardCoverFile, userInfo) => {
       };
 
       updatedCard = await cardModel.unshiftNewComment(cardId, commentData);
+    } else if (updateData.incomingMemberInfo) {
+      // Trường hợp ADD hoặc REMOVE thành viên ra khỏi Card
+      updatedCard = await cardModel.updateMembers(cardId, updateData.incomingMemberInfo);
     } else {
       // Các trường hợp update chung như title, description
       updatedCard = await cardModel.update(cardId, updateData, cardCoverFile);
